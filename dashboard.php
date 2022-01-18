4ee337c0c1c7b48dde20b68f798adc429e27cf4a
Matas Grybauskas <grybauskas.matas@gmail.com>
2022-01-18 18:34:21 +0200

    added dashboard main section styling

--- dashboard.php
+++ dashboard.php
@@ -9,6 +9,34 @@
     <title>FinSek - Pagrindinis</title>
 </head>
 <body>
-    
+    <div class="flex-row-container">
+        <div class="sidebar">
+            <div class="sidebar-header">
+                <h1><object data="svg/Financial_Document.svg" height="50rem"></object>FinSek</h1>
+            </div>
+            <div class="sidebar-body">
+                <h2><object data="svg/Certificate.svg" height="45rem"></object><a href="#">Pagrindinis</h2>
+                <h2><object data="svg/Cash.svg" height="45rem"></object><a href="#">Pavedimai</h2>
+                <h2><object data="svg/Credit_Card.svg" height="45rem"></object><a href="#">Sąskaitos</h2>
+                <h2><object data="svg/Money_Bag.svg" height="45rem"></object><a href="#">Paskolinimai</h2>
+                <h2><object data="svg/Currency.svg" height="45rem"></object><a href="#">Pasiskolinimai</h2>
+            </div>
+        </div>
+        <div class="main">
+            <div class="main-row">
+                <div class="card linechart">
+                    <h2>Linegraph</h2>
+                </div>    
+            </div>
+            <div class="main-row">
+                <div class="card piechart">
+                    <h2>Pie chart</h2>
+                </div>
+                <div class="card txtstats">
+                    <h2>Text stats</h2>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
