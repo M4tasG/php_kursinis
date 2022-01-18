4d04344172f91b1e8d49151e687400015a36adfb
Matas Grybauskas <grybauskas.matas@gmail.com>
2022-01-18 16:43:59 +0200

    finished splash page structure and styling

--- index.php
+++ index.php
@@ -9,8 +9,34 @@
     <title>FinSek</title>
 </head>
 <body>
-    <div class="container">
-        <h1>FinSek</h1>
+    <div class="splash-container">
+        <div class="splash-header">
+            <h1><object data="svg/Financial_Document.svg" height="50rem"></object>FinSek</h1>
+            <h3>Aplikacija jūsų biudžetui sekti</h3>
+        </div>
+        <div class="splash-main">
+            <h1>Pradėkite naudotis dabar!</h1>
+            <button class="btn"><a href="login.php" class="txt-white">Prisijungti</a></button>
+            <button class="btn-alternate"><a href="register.php" class="txt-accent">Registruotis</a></button>
+            <div class="splash-cards">
+                <div class="card splash-card">
+                    <h3>Patogumas</h3>
+                    <object data="svg/Currency.svg" height="100rem"></object>
+                    <p>Visa informacija ir statistika apie jūsų išlaidas, sąskaitas ir kita vienoje vietoje! Naudodamiesi šia aplikacija galėsite sekti savo finansus ir daryti pakeitimus kaip niekada lengvai!</p>
+                </div>
+                <div class="card splash-card">
+                    <h3>Nauda</h3>
+                    <object data="svg/Money_Bag.svg" height="100rem"></object>
+                    <p>Dėka suprantamai pateikiamos ir vienoje vietoje esančios statistikos koreguoti savo išlaidas ir kitus finansinius pasirinkimus tampa labai lengva!</p>
+                </div>
+                <div class="card splash-card">
+                    <h3>Jokių išlaidų</h3>
+                    <object data="svg/Piggy_Bank.svg" height="100rem"></object>
+                    <p>Norint sutaupyti pirma turite neišleisti. Todėl vadovaudamiesi šiuo mastymu padarėme aplikaciją visiškai nemokamą naudoti!</p>
+                </div>
+            </div>
+            <footer>All rights reserved @ PHP_KURSINIS_DARBELIS</footer>
+        </div>
     </div>
 </body>
 </html>
