56946ca0e50fa336407e0501597d8dff52cf7dad
M4tasG <grybauskas.matas@gmail.com>
2022-01-26 23:16:17 +0200

    added validation checks in registration

--- register_check.php
+++ register_check.php
@@ -6,8 +6,16 @@
         $username = $_POST['username'];
         $email = $_POST['email'];
         $password = hash_password($_POST['password']);
-        $query = "INSERT INTO users (`username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
-        $dbconnect -> query($query);
-        header("Location: login.php");
+        if(!@check_for_dupe_username($username) && !@check_for_dupe_email($email)){
+            $clean_credentials = validate_register_creds($username, $email);
+            $username = $clean_credentials['username'];
+            $email = $clean_credentials['email'];
+            $query = "INSERT INTO users (`username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
+            $dbconnect -> query($query);
+            header("Location: login.php");
+        }
+        else{
+            echo "<div class='change-message error'>Error: Username or email already exists</div>";
+        }
     }
 ?>
