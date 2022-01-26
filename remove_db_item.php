cf0d39e291450c55a1f3d242ede8ad9a7678b343
M4tasG <grybauskas.matas@gmail.com>
2022-01-27 00:54:11 +0200

    added logic to reset the balance to previous state when deleting items

--- remove_db_item.php
+++ remove_db_item.php
@@ -5,7 +5,13 @@
         include 'functions.php';
         $o_name = $_POST['name'];
         $o_user = fetch_user_id($_SESSION['username']);
+        $query = "SELECT * FROM transactions WHERE name = '$o_name' AND user = '$o_user'";
+        $result = $dbconnect->query($query);
+        $entry = $result->fetch_assoc();
+        $o_amount = $entry['amount'];
         $query = "DELETE FROM transactions WHERE name = '$o_name' AND user = '$o_user'";
+        $dbconnect -> query($query);
+        $query = "UPDATE accounts SET amount = amount - '$o_amount' WHERE user = '$o_user'";
         $dbconnect -> query($query);
         header("Location: transactions.php");
     }
@@ -23,7 +29,13 @@
         include 'functions.php';
         $o_name = $_POST['name'];
         $o_user = fetch_user_id($_SESSION['username']);
+        $query = "SELECT * FROM lends WHERE name = '$o_name' AND user = '$o_user'";
+        $result = $dbconnect->query($query);
+        $entry = $result->fetch_assoc();
+        $o_amount = $entry['amount'];
         $query = "DELETE FROM lends WHERE name = '$o_name' AND user = '$o_user'";
+        $dbconnect -> query($query);
+        $query = "UPDATE accounts SET amount = amount + '$o_amount' WHERE user = '$o_user'";
         $dbconnect -> query($query);
         header("Location: lends.php");
     }
@@ -32,7 +44,13 @@
         include 'functions.php';
         $o_name = $_POST['name'];
         $o_user = fetch_user_id($_SESSION['username']);
+        $query = "SELECT * FROM borrows WHERE name = '$o_name' AND user = '$o_user'";
+        $result = $dbconnect->query($query);
+        $entry = $result->fetch_assoc();
+        $o_amount = $entry['amount'];
         $query = "DELETE FROM borrows WHERE name = '$o_name' AND user = '$o_user'";
+        $dbconnect -> query($query);
+        $query = "UPDATE accounts SET amount = amount - '$o_amount' WHERE user = '$o_user'";
         $dbconnect -> query($query);
         header("Location: borrows.php");
     }
