c1cfe66eed245fd2dd63a61d1ba5db655939e376
M4tasG <grybauskas.matas@gmail.com>
2022-01-26 17:52:51 +0200

    added basic login check logic

--- login_check.php
+++ login_check.php
@@ -1,5 +1,24 @@
 <?php
-    function hash_password($password){
-        return md5($password);
+    include 'db.php';
+    include 'functions.php';
+    if(isset($_POST['submit'])){
+        echo "Submit detected";
+        $username = $_POST['username'];
+        $password = hash_password($_POST['password']);
+        $query = "SELECT * FROM users WHERE username = '$username'";
+        $result = $dbconnect -> query($query);
+        $entry = $result->fetch_assoc();
+        if(check_creds($entry, $username, $password)){
+            session_start();
+            $_SESSION['authenticated'] = true;
+            $_SESSION['username'] = $username;
+        }
+        if(!$dbconnect -> query($query)){
+            echo "<div class='change-message error'>Error: " . $dbconnect -> error . "</div>";
+        }
+        else{
+            echo "<div class='change-message success'>User added successfully</div>";
+        }
+        header("Location: dashboard.php");
     }
 ?>
